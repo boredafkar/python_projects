ce2ce6131e92561ae99b55d135715972ff710ffa
boredafkar <30960306+boredafkar@users.noreply.github.com>
2017-08-16 13:52:24 +0530

    Create reversetext.py

---
+++ reversetext.py
@@ -0,0 +1,9 @@
+#Why does this not work?
+
+def reverse(text):
+  reversetext = ""
+  for char in text in range(len(text) - 1, 0):
+    reversetext = reversetext + i
+  return reversetext
+  
+#is "char in text in range" acceptable syntax?
